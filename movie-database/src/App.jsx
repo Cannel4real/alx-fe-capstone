1030087843b8d4630356874f5fdccc14414b2c06
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-12-06 16:05:28 +0200

    bug fix

--- movie-database/src/App.jsx
+++ movie-database/src/App.jsx
@@ -5,9 +5,18 @@ function App() {
 
   return (
     <>
-    <div  className="text-blue-500 !important">
-  Now this text will be blue.
+    <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+  <div class="text-center px-6 sm:px-12">
+    <h1 class="text-4xl sm:text-6xl font-extrabold leading-tight mb-4">
+      Welcome to <span class="text-indigo-500">Cannel</span>
+    </h1>
+    <p class="text-lg sm:text-xl font-light mb-6">
+      Discover, explore, and save your favorite movies. From classics to the latest blockbusters, we've got it all!
+    </p>
+
+  </div>
 </div>
+
 
     </>
     )
