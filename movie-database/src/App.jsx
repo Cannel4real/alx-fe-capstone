aea57f42631217e87a39dbde876fe8934a59a447
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-12-06 12:59:36 +0200

    init my new app

---
+++ movie-database/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div  className="text-blue-500 !important">
+  Now this text will be blue.
+</div>
+
+    </>
+    )
+}
+
+export default App
