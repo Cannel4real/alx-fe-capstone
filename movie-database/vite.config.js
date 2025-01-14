8914892f455527c703ad167877696f4a4014e610
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2025-01-15 01:10:37 +0200

    bug fix

--- movie-database/vite.config.js
+++ movie-database/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for assets
   build: {
-    outDir: 'dist', // This should be where your build files are output
-  }
+    outDir: 'dist', // Output directory for build files
+  },
 });
